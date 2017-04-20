dec62503d470d5829dc93c5730c696a515d26085
zengda <zengda@yunba.io>
2017-04-20 20:15:57 +0800

    add some more blog

--- anti-debug/ptrace.c
+++ anti-debug/ptrace.c
@@ -1,11 +1,16 @@
 #include <sys/types.h>
 #include <sys/ptrace.h>
+#include <unistd.h>
 #include <stdio.h>
 
 int main(int argc, const char *argv[])
 {
     
     ptrace(PT_DENY_ATTACH, 0, NULL, 0);
-    printf("hello world, you can't debug me");
+    printf("hello world, you can't debug me\n");
+    while(1){
+        printf("hello world, you can't debug me\n");
+        sleep(1);
+    }
     return 0;
 }
